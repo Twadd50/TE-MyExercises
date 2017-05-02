0e16e2db1919b5265a7ca23b3489447bdeaeb990
Tyler Waddington <twadd50@gmail.com>
2017-05-02 17:14:56 -0400

    Still working on day 2 exercises

--- m1-w1d2-variables-and-datatypes-exercises/src/main/java/com/techelevator/Exercises.java
+++ m1-w1d2-variables-and-datatypes-exercises/src/main/java/com/techelevator/Exercises.java
@@ -7,51 +7,95 @@ public class Exercises {
         /* 
         1. 4 birds are sitting on a branch. 1 flies away. How many birds are left on
         the branch? 
-        */
-
+        */ 
+		int birdsOnABranch = 4 ;
+        int birdsFlewAway = 1;
+        int totalNumberOfBirds = birdsOnABranch - birdsFlewAway;
+        System.out.println("Total Number of birds is equal to: " +totalNumberOfBirds);
+		
+        
         /* 
         2. There are 6 birds and 3 nests. How many more birds are there than
-        nests? 
+        nests?
         */
+        int birds = 6;
+        int nests = 3;
+        int birdsAndNests = birds - nests;
+        System.out.println("There are this many more birds than nests is equal to: " + birdsAndNests);
+
 
         /* 
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods? 
         */
+        int racoonsPlaying = 3;
+        int racoonsGoingHome = 2;
+        int racoonsLeftInWoods = racoonsPlaying - racoonsGoingHome;
+        System.out.println("There are \"" + racoonsLeftInWoods + "\" racoons left in the woods.");   
 
         /* 
         4. There are 5 flowers and 3 bees. How many less bees than flowers? 
         */
-
+        int flowers = 5;
+        int bees = 3;
+        int beesLessThanFlowers = flowers - bees;
+        System.out.println("There are \"" + beesLessThanFlowers +"\" less bees than flowers");
+        
         /* 
         5. 1 lonely pigeon was eating breadcrumbs. Another pigeon came to eat
         breadcrumbs, too. How many pigeons are eating breadcrumbs now? 
         */
+        int lonelyPigeon = 1;
+        int anotherPigeon = 1;
+        int totalNumberOfPigeons = lonelyPigeon + anotherPigeon;
+        System.out.println("There are now \"" + totalNumberOfPigeons +"\" pigeons eating breadcrumbs");
+        
+        
 
         /* 
         6. 3 owls were sitting on the fence. 2 more owls joined them. How many
         owls are on the fence now? 
         */
+        int owlsOnFence = 3;
+        int arrivingOwls = 2;
+        int totalNumberOfOwls = owlsOnFence + arrivingOwls;
+        System.out.println("There are now this many owls on the fence: " + totalNumberOfOwls);
 
         /* 
         7. 2 beavers were working on their home. 1 went for a swim. How many
         beavers are still working on their home? 
         */
-
+        int beaversWorkingOnHome = 2;
+        int beaversGoingForASwim = 1;
+        int beaversNumber = beaversWorkingOnHome - beaversGoingForASwim;
+        System.out.println(" There are this many beavers working on their home " + beaversNumber);
         /* 
         8. 2 toucans are sitting on a tree limb. 1 more toucan joins them. How
         many toucans in all? 
         */
+        int toucansOnTree = 2;
+        int joiningToucan = 1;
+        int totalToucanNumber = toucansOnTree + joiningToucan;
+        System.out.println("There are \""  + totalToucanNumber +"\" toucans in all");
 
         /* 
         9. There are 4 squirrels in a tree with 2 nuts. How many more squirrels
         are there than nuts? 
         */
+        int numberOfSquirrels = 4;
+        int numberOfNuts = 2;
+        int squirrelsVsNuts = numberOfSquirrels - numberOfNuts;
+        System.out.println("There are \"" + squirrelsVsNuts + "\" more squirrels than there are nuts ");
 
         /* 
         10. Mrs. Hilt found a quarter, 1 dime, and 2 nickels. How much money did
         she find? 
         */
+        int dime = 10;
+        int quarter = 25;
+        int twoNickels = 10;
+        int amountOfMoneyFound = dime + quarter + twoNickels;
+        System.out.println("Mr.s Hilt found \"" + amountOfMoneyFound + "\" cents");
 
         /* 
         11. Mrs. Hilt's favorite first grade classes are baking muffins. Mrs. Brier's
@@ -59,33 +103,58 @@ public class Exercises {
         Mrs. Flannery's class bakes 17 muffins. How many muffins does first
         grade bake in all? 
         */
-
+        int mrsBriersClass = 18;
+        int mrsMacadamsClass = 20;
+        int mrsFlanerysClass = 17;
+        int totalMuffinCount = mrsBriersClass + mrsMacadamsClass + mrsFlanerysClass;
+        System.out.println("The first grade class baked \"" + totalMuffinCount + "\" muffins overall");
         /*
         12. Mrs. Hilt bought a yoyo for 24 cents and a whistle for 14 cents. How
         much did she spend in all for the two toys?
         */
+        int yoyo = 24;
+        int whistle = 14;
+        int totalPrice = yoyo + whistle;
+        System.out.println("Mrs. Hilt spent a total of \"" + totalPrice + "\" cents on the two toys");
         
         /*
         13. Mrs. Hilt made 5 Rice Krispie Treats. She used 8 large marshmallows
         and 10 mini marshmallows.How many marshmallows did she use
         altogether?
         */
+        int miniMallows = 10;
+        int largeMallows = 8;
+        int totalMallows = miniMallows + largeMallows;
+        System.out.println("Mrs. Hilt used \"" + totalMallows + "\" ");
         
         /*
         14. At Mrs. Hilt's house, there was 29 inches of snow, and Brecknock
         Elementary School received 17 inches of snow. How much more snow
         did Mrs. Hilt's house have?
         */
+        int house = 29;
+        int school = 17;
+        int snowfall = school - house;
+        System.out.println("Mrs. Hilts house had \"" + snowfall + "\" more inches of snow ");
         
         /*
         15. Mrs. Hilt has $10. She spends $3 on a toy truck and $2 on a pencil
         case. How much money does she have left?
-        */
+         */
+        int money = 10;
+        int truck = 3;
+        int pencil = 2;
+        int totalLeftOver = money - truck -pencil;
+        System.out.println("Mrs. Hilt has $ \"" + totalLeftOver + " left over ");
         
         /*
         16. Josh had 16 marbles in his collection. He lost 7 marbles. How many
         marbles does he have now?
         */
+        int marbles = 16;
+        int marblesLost = 7;
+        int totalMarbles = marbles - marblesLost;
+        System.out.println("Josh now has \"" + totalMarbles + "\" left over ");
         
         /*
         17. Megan has 19 seashells. How many more seashells does she need to
